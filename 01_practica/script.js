f601cff37415ae8dab094728819f332d8664b1a1
PC156\Alumno <pacomateostomas@gmail.com>
2023-12-21 11:00:59 +0100

    made the button work

--- 01_practica/script.js
+++ 01_practica/script.js
@@ -1,5 +1,25 @@
 console.log("holy script connected")
+var conta = 0
+console.log(conta)
 
 function pulsador() {
     console.log("consecrated input detected")
+    document.querySelector("#pulsada").innerHTML = "Has pulsado el boton"
+    conta += 1
+    console.log(conta)
+    if (conta == 2) {
+        document.querySelector("#pulsada").innerHTML = "Has vuelto a pulsado el boton" 
+    }
+    if (conta == 3) {
+        document.querySelector("#pulsada").innerHTML = "HAS VUELTO A PULSAR EL BOTON!" 
+    }
+    if (conta == 4) {
+        document.querySelector("#pulsada").innerHTML = "HAS VUELTO A PULSAR EL BOTON!" 
+        document.querySelector("#plusada").style.color = "white";
+        document.querySelector("#plusada").style.background = "black";
+    }
+    if (conta == 5) {
+        document.querySelector("#pulsada").innerHTML = "Has pulsado el boton" 
+        conta = 1
+    }
 }
